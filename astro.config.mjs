eec7d1abfa43bf63640d626dd551643dc56fb136
Sergio <secwinix@gmail.com>
2024-06-15 20:17:58 +0200

    rollback

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
   server: {
     port: 4321,
   },
