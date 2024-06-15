aa13ed427f8ed5dbe56b36ade24e10d9f23a3757
Sergio <secwinix@gmail.com>
2024-06-15 20:09:48 +0200

    fix routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  output: 'server',
   server: {
     port: 4321,
   },
