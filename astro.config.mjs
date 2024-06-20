11cc755ce66161f4012af43b101cb8bef35cff87
Sergio <secwinix@gmail.com>
2024-06-20 17:33:05 +0200

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+<<<<<<< HEAD
   integrations: [tailwind(), react()],
   output: 'server',
+=======
+  integrations: [tailwind()],
+>>>>>>> parent of dfde34a (update charts)
   server: {
     port: 4321,
   },
